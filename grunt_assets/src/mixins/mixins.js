b69d8acf7e892ded81bb9014afc6265126702c45
carlosmtx <cmcmteixeira@gmail.com>
2015-08-10 01:04:08 +0100

    [Added] File Extension Categorizing

--- grunt_assets/src/mixins/mixins.js
+++ grunt_assets/src/mixins/mixins.js
@@ -1,14 +1,27 @@
-var _ = require('lodash');
+var _   = require('lodash'),
+    path= require('path');
+
+function getFileType(path,mappings){
+
+}
 
 
-module.exports.getExtension= function getFileExtension(path){
-
+module.exports.getFileType = function getFileType(paths,mappings){
+    var fileType = [];
+    if (_.isArray(paths)) paths = [paths];
+    _.forEach(paths,function(val){
+        var extension = path.extname(val);
+        var type = '';
+        _.forEach(mappings,function(val,key){
+            if (_.include(val,extension)) fileType.push(extension);
+            return false;
+        });
+    });
+    console.log(fileType);
+    var extension = _.union(fileType);
+    if( extension.length === 0) throw 'Invalid extensions found';
+    if( extension.length >   1) throw 'Multiple fileTypes in the same asset';
+    console.log(extension);
+    return extension[0];
 };
-module.exports.getFileType = function getFileType(mappins,extension){
-
-};
-
-
-
-
 return module.exports;
